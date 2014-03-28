aa24c2a24ac907e6447439fed8c01f0bb1399d34
Sumbhav <sumbhav.31161@gmail.com>
2014-03-28 14:56:35 -0700

    Deleted High Sumbhav comment because I'm not high.

--- src/personalhealthtracker/PersonalHealthTracker.java
+++ src/personalhealthtracker/PersonalHealthTracker.java
@@ -9,7 +9,7 @@ public class PersonalHealthTracker
     // This is the driver class. Display the login view and give control to the user.
     public static void main(String[] args)
     {
-        // Comment High Sumbhav
+        
         LoginView loginView = new LoginView();
         loginView.setVisible(true);
         LoginController loginController = new LoginController(loginView);
